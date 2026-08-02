68527e3586daf54626c340991f5f779e68a510c1
Logesh-bruce <logeshmg432006@gmail.com>
2026-08-02 12:14:51 +0530

    feat(jobs): add JobSkillResponse DTO

---
+++ src/main/java/com/hireflow/dto/response/JobSkillResponse.java
@@ -0,0 +1,16 @@
+package com.hireflow.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class JobSkillResponse {
+
+    private String name;
+    private boolean required;
+}
