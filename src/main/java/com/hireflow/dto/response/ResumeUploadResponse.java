5d6a191240441300003d73e874899bd25c76a987
Logesh-bruce <logesh@hireflow.ai>
2026-08-02 12:41:44 +0530

    feat(resume): add ResumeUploadResponse DTO

---
+++ src/main/java/com/hireflow/dto/response/ResumeUploadResponse.java
@@ -0,0 +1,20 @@
+package com.hireflow.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ResumeUploadResponse {
+
+    private UUID resumeId;
+    private String fileName;
+    private String parseStatus;
+    private String message;
+}
